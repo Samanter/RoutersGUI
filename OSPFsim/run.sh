f1f808fe14b43a5d87129777287d594111749c82
Samanter <55460198+Samanter@users.noreply.github.com>
2023-10-03 11:07:49 +0000

    Delete OSPFsim directory

--- OSPFsim/run.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-cd /media/hdd/UTP/2023-2/Curso\ Integrador\ 1:\ Sistemas-Software/Proyecto\ 1/RoutersGUI/OSPFsim/src/
-
-# Compile the Java source files
-javac -cp . UI/Interface/Main.java
-
-# Run the Java application
-java -cp . UI.Interface.Main
